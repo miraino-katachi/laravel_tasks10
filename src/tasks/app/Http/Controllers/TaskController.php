3d603722eb69e863ab81969d3f950769e7b78948
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2025-02-03 10:12:44 +0900

    TaskController#create()を引数なしに修正

--- src/tasks/app/Http/Controllers/TaskController.php
+++ src/tasks/app/Http/Controllers/TaskController.php
@@ -26,7 +26,7 @@ class TaskController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Task $task)
+    public function create()
     {
         return view('tasks.create');
     }
